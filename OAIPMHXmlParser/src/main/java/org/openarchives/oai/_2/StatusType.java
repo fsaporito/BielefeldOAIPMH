5fa2d3615ab1c3910ed960b722934d2077a3f497
fsaporito <sap93@gmail.com>
2021-11-06 17:50:52 +0100

    Update xml data classes

--- OAIPMHXmlParser/src/main/java/org/openarchives/oai/_2/StatusType.java
+++ OAIPMHXmlParser/src/main/java/org/openarchives/oai/_2/StatusType.java
@@ -2,7 +2,7 @@
 // Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.3.0.1 
 // Vedere <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2021.10.30 alle 01:51:51 PM UTC 
+// Generato il: 2021.11.06 alle 03:53:06 PM UTC 
 //
 
 
