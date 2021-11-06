b0616d8a612a6adffd5fea8c4af6e3ce67853982
fsaporito <sap93@gmail.com>
2021-11-07 00:43:54 +0100

    Add H2 in-memmory database

---
+++ restBackend/src/main/resources/data.sql
@@ -0,0 +1,48 @@
+INSERT INTO RECORDS_X_AUTHOR (authorName, recordNum) VALUES
+  ('Duck, Donald', 10),
+  ('Duck, Daisy', 11),
+  ('McDuck, Scrudge', 5);
+
+
+INSERT INTO RECORDS_X_ORCID_AUTHOR (authorName, recordNum) VALUES
+  ('00000', 10),
+  ('00001', 11),
+  ('00002', 5);
+
+
+INSERT INTO RECORDS_X_PUBLICATION_YEAR (publicationYear, recordNum) VALUES
+  (2009, 10),
+  (2010, 11),
+  (2011, 10),
+  (2012, 11),
+  (2013, 10),
+  (2014, 11),
+  (2015, 10),
+  (2016, 11),
+  (2017, 10),
+  (2018, 11),
+  (2019, 10),
+  (2020, 11),
+  (2021, 10);
+
+
+INSERT INTO RECORDS_X_TYPOLOGY (typology, recordNum) VALUES
+  ('JOURNAL', 10),
+  ('THESIS', 11),
+  ('CONFERENCE', 5);
+
+
+INSERT INTO RECORDS_JOURNALS_X_PUBLICATION_YEAR (publicationYear, recordNum) VALUES
+  (2009, 10),
+  (2010, 11),
+  (2011, 10),
+  (2012, 11),
+  (2013, 10),
+  (2014, 11),
+  (2015, 10),
+  (2016, 11),
+  (2017, 10),
+  (2018, 11),
+  (2019, 10),
+  (2020, 11),
+  (2021, 10);
