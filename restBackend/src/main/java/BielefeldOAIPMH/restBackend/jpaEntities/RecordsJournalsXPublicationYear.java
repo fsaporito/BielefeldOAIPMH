385faf68179428ecbd5eeef9f1710dafeb4d9537
fsaporito <sap93@gmail.com>
2021-11-07 02:44:34 +0100

    Add missing entity annotation

--- restBackend/src/main/java/BielefeldOAIPMH/restBackend/jpaEntities/RecordsJournalsXPublicationYear.java
+++ restBackend/src/main/java/BielefeldOAIPMH/restBackend/jpaEntities/RecordsJournalsXPublicationYear.java
@@ -1,9 +1,11 @@
 package BielefeldOAIPMH.restBackend.jpaEntities;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class RecordsJournalsXPublicationYear {
 
 	@Id
